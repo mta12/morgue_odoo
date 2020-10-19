b730b1f12916d1a049eaa12f09913eedf5b6683a
mta12 <tobiemanga@gmail.com>
2020-10-19 11:09:35 +0100

    modif du manifeste et ajout de l'icone

--- __manifest__.py
+++ __manifest__.py
@@ -1,22 +1,25 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "morgue_cmr",
+    'name': "Hos-Morgue",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+        Module de gestion d'une Morgue d'hopital""",
 
     'description': """
-        Long description of module's purpose
+        Gestion de la Morgue: 
+        - gestion des défunts et de leurs responsables 
+        - gestion des coffrets pour cadavres
+        - gestion des opérations financières y associées
+        - Etablissement des bilans financiers
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'author': "Powered by MTA (Manga Tobie A.)",
+    'website': "https://github.com/mta12/morgue_odoo",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
+    'category': 'Mortuary',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
@@ -32,4 +35,5 @@
     'demo': [
         'demo/demo.xml',
     ],
+    'application': True,
 }
